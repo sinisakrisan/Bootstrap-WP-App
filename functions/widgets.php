1d46a5bbaa2ffe45fb3a066196ee8cbccc1f5be0
krisansinisa <krisansinisa@gmail.com>
2019-08-21 10:42:59 +0200

    Update widgets.php

--- functions/widgets.php
+++ functions/widgets.php
@@ -70,3 +70,61 @@ function app_widgets_init() {
 
 }
 add_action( 'widgets_init', 'app_widgets_init' );
+
+
+//
+//  Custom options for widgets
+//
+/*
+function app_in_widget_form($t,$return,$instance){
+  
+  $instance = wp_parse_args( (array) $instance, array( 'title' => '', 'text' => '', 'style' => '') );
+  if ( !isset($instance['style']) )
+      $instance['style'] = null;
+  ?>
+  <hr>
+  <h3>Widget options:</h3>
+  <div style="background:#eee; padding:10px; margin-bottom:10px;">
+      <label for="<?php echo $t->get_field_id('style'); ?>">Widget style:</label>
+      <select id="<?php echo $t->get_field_id('style'); ?>" name="<?php echo $t->get_field_name('style'); ?>">
+          <option <?php selected($instance['style'], 'none');?> value="none">none</option>
+          <option <?php selected($instance['style'], 'box');?>value="box">Box</option>
+          <option <?php selected($instance['style'], 'bordered');?> value="bordered">Bordered</option>
+          <option <?php selected($instance['style'], 'gradient');?> value="gradient">Gradient</option>
+      </select>
+  </div>
+  <?php
+  $retrun = null;
+  return array($t,$return,$instance);
+  
+}
+
+function app_in_widget_form_update($instance, $new_instance, $old_instance){
+  $instance['style'] = $new_instance['style'];
+  return $instance;
+}
+
+function app_dynamic_sidebar_params($params){
+  global $wp_registered_widgets;
+  $widget_id = $params[0]['widget_id'];
+  $widget_obj = $wp_registered_widgets[$widget_id];
+  $widget_opt = get_option($widget_obj['callback'][0]->option_name);
+  $widget_num = $widget_obj['params'][0]['number'];
+
+  if(isset($widget_opt[$widget_num]['style']))
+    $style = 'widget--' . $widget_opt[$widget_num]['style'];
+  else
+    $style = '';
+    $params[0]['before_widget'] = preg_replace('/class="/', 'class="'.$style.' ',  $params[0]['before_widget'], 1);
+  return $params;
+}
+
+
+//Add input fields(priority 5, 3 parameters)
+add_action('in_widget_form', 'app_in_widget_form',5,3);
+//Callback function for options update (priorität 5, 3 parameters)
+add_filter('widget_update_callback', 'app_in_widget_form_update',5,3);
+//add class names (default priority, one parameter)
+add_filter('dynamic_sidebar_params', 'app_dynamic_sidebar_params');
+
+*/
